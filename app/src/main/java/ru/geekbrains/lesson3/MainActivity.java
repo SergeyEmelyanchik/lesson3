15acfa931603841e675c5ccf5643d62cef979118
Sergey <emelyanchik1980@gmail.com>
2022-01-30 14:49:33 +0300

    Добавил метод onClick в MainActivity.java

--- app/src/main/java/ru/geekbrains/lesson3/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lesson3/MainActivity.java
@@ -16,25 +16,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Screen=findViewById(R.id.screen);
-        Del=findViewById(R.id.del);
-        Clear=findViewById(R.id.clear);
-        Equal=findViewById(R.id.equal);
-        Div=findViewById(R.id.div);
-        Multiply=findViewById(R.id.multiply);
-        Minus=findViewById(R.id.minus);
-        Plus=findViewById(R.id.plus);
-        Dot=findViewById(R.id.dot);
-        Zero=findViewById(R.id.zero);
-        Nine=findViewById(R.id.nine);
-        Eight=findViewById(R.id.eight);
-        Seven=findViewById(R.id.seven);
-        Six=findViewById(R.id.six);
-        Five=findViewById(R.id.five);
-        Four=findViewById(R.id.four);
-        Three=findViewById(R.id.three);
-        Two=findViewById(R.id.two);
-        One=findViewById(R.id.one);
+        Screen = findViewById(R.id.screen);
+        Del = findViewById(R.id.del);
+        Clear = findViewById(R.id.clear);
+        Equal = findViewById(R.id.equal);
+        Div = findViewById(R.id.div);
+        Multiply = findViewById(R.id.multiply);
+        Minus = findViewById(R.id.minus);
+        Plus = findViewById(R.id.plus);
+        Dot = findViewById(R.id.dot);
+        Zero = findViewById(R.id.zero);
+        Nine = findViewById(R.id.nine);
+        Eight = findViewById(R.id.eight);
+        Seven = findViewById(R.id.seven);
+        Six = findViewById(R.id.six);
+        Five = findViewById(R.id.five);
+        Four = findViewById(R.id.four);
+        Three = findViewById(R.id.three);
+        Two = findViewById(R.id.two);
+        One = findViewById(R.id.one);
+    }
+
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String data = button.getText().toString();//объявляем строковые данные для получения текста кнопок
+        switch (data) {
+            case "clear":
+                input = "";
+                break;
+            case "del":
+                String newText = input.substring(0, input.length() - 1);
+                input = newText;
+                break;
+            case "equal":
+                Result();
+                Answer = input;
+                break;
+            case "multiply":
+                Result();
+                input = "*";
+                break;
+            default:
+                if (input == null) {
+                    input = "";
+                }
+                if (data.equals("+") || data.equals("-") || data.equals("/")) {
+                    Result();
+                }
+                input += data;
+
+        }
+Screen.setText(input);
+    }
+
+    private void Result() {
 
     }
 }
